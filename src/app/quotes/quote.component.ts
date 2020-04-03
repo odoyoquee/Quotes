bc4d82006bf128f75eedeba4e58334d335fbc297
odoyoquee <odoyograce23@gmail.com>
2020-04-03 09:25:24 +0300

    added styles

---
+++ src/app/quotes/quote.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote';
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+  quotes=[new Quote("KLO","Sandy Meres","Trust the Process"),
+new Quote("Abraham Lincoln","Abraham Lincoln","Give me six hours to cut down a tree and i'll spend the first four sharpening the axe ")]
+  preNum:number
+  lastNum:number
+  counter:number
+
+  addQuote(emittedQuote){
+    this.quotes.push(emittedQuote)
+  }
+
+  upvote(i){
+      this.quotes[i].upvotes+=1
+  }
+  downvote(i){
+    this.quotes[i].downvotes+=1
+  }
+  delQuote(i){
+    this.quotes.splice(i, 1)
+  }
+  highestUpvote(){
+    this.preNum = 0
+    this.lastNum = 0
+
+    for(this.counter=0 ; this.counter < this.quotes.length; this.counter++) {
+      this.lastNum = this.quotes[this.counter].upvotes;
+      if(this.lastNum > this.preNum){this.preNum = this.lastNum}
+    }
+    return  this.preNum
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
